47bc20ee3e63a2042ac3988a7468476336837088
nickzylstra <nick.zylstra@gmail.com>
2019-11-04 12:36:02 -0800

    Completes first two sections

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-// TODO
+/* eslint-disable no-undef */
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/jsx-filename-extension */
+// import React from 'react';
+// import ReactDOM from 'react';
+
+const GroceryList = (props) => (
+  <div GroceryList>
+    <GroceryListItem item={`${props.groceryItems[0]}`} />
+    <GroceryListItem item={`${props.groceryItems[1]}`} />
+    <Avocados />
+    <Apples />
+  </div>
+);
+
+const Apples = () => (
+  <ul>apples</ul>
+);
+
+const Avocados = () => (
+  <ul>avocados</ul>
+);
+
+const GroceryListItem = (props) => (
+  <ul>{props.item}</ul>
+);
+
+ReactDOM.render(<GroceryList groceryItems={['apples', 'bananas']} />, document.getElementById('app'));
